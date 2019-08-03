1ba91f4235eacfd0bf4c687b78e8f1ccf96a5d6e
Dominic <dominic.m.chu@gmail.com>
2019-08-03 03:05:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-Object.assign({}, object, { key: value});
+var object = new Object({ key: value })
+object.key = value;
 }
